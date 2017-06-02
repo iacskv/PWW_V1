a97685fb5284a9cca9429b7d276d2dd2f899ae96
skv <skvaltell@yandex.ru>
2017-06-02 13:40:40 +0300

    добавлена модель акта рождения

--- pww-tests/src/test/java/qa/pww/models/BornData.java
+++
@@ -1,9 +0,0 @@
-package qa.pww.models;
-
-/**
- * Created by k.smotrov on 02.06.2017.
- */
-public class BornData {
-
-    
-}
---
+++ pww-tests/src/test/java/qa/pww/models/BornDataMainFields.java
@@ -0,0 +1,97 @@
+package qa.pww.models;
+
+/**
+ * Created by k.smotrov on 02.06.2017.
+ */
+public class BornDataMainFields {
+    //поля данных об а/з
+    String agsNum;
+    String liter;
+    String agsDd;
+    String agsmm;
+    String agsYyyy;
+    String zagsAgsName;
+    String agsCopy;
+
+    //поля сведения о ребенке (1-8)
+    String childSex;
+    String childLastName;
+    String childFirstName;
+    String childMiddleName;
+    String childDdBorn;
+    String childMmBorn;
+    String childYyyyBorn;
+    String childCountryNoClass;
+    String childRegionNoClass;
+    String childCountry;
+    String childRegion;
+    String childDisnrict;
+    String childPunkt;
+    String childBirthCond;
+
+    //основание восстановления а/з (10)
+    String vosstDd;
+    String vosstMm;
+    String vosstYyyy;
+    String vosstOrg;
+
+    //сведения об отце (11-17)
+    String fatherLastName;
+    String fatherFirstName;
+    String fatherMiddleName;
+    String fatherDdBorn;
+    String fatherMmBorn;
+    String fatherYyyyBorn;
+    String fatherCountryNoClass;
+    String fatherRegionNoClass;
+    String fatherCountry;
+    String fatherRegion;
+    String fatherDistrict;
+    String fatherPunkt;
+    String fatherCitizenshipNoClass;
+    String fatherCitizenship;
+    String fatherNationalNoClass;
+    String fatherNational;
+
+    //сведения о матери (11-17)
+    String matherLastName;
+    String matherFirstName;
+    String matherMiddleName;
+    String matherDdBorn;
+    String matherMmBorn;
+    String matherYyyyBorn;
+    String matherCountryNoClass;
+    String matherRegionNoClass;
+    String matherCountry;
+    String matherRegion;
+    String matherDistrict;
+    String matherPunkt;
+    String matherCitizenshipNoClass;
+    String matherCitizenship;
+    String matherNationalNoClass;
+    String matherNational;
+
+    //основание сведений об отце (19)
+
+    String baseTypeDoc;
+    String baseNumDoc;
+    String baseDdDoc;
+    String baseMmDoc;
+    String baseYyyyDoc;
+
+    //выдано свидетельство (21)
+
+    String certSeria;
+    String certNum;
+    String certDd;
+    String certMm;
+    String certYyyy;
+
+    //есть Изменения?
+    String moreStage;
+
+
+
+
+
+}
